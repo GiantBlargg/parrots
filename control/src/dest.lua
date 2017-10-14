9965c897486f98fd2432571e74eec842eb7ac923
Daniel Doran <ddoran3@gmail.com>
2017-10-13 23:32:44 -0600

    local vars on dest

--- control/src/dest.lua
+++ control/src/dest.lua
@@ -1,9 +1,9 @@
 
-CHANNEL_ANNOUNCE = 1000
+local CHANNEL_ANNOUNCE = 1000
 local modem = peripheral.wrap("back")
 modem.open(CHANNEL_ANNOUNCE)
 
-dest=...
+local dest=...
 
-event,side,senderCh,replyCh,message,distance=os.pullEvent("modem_message")
+local event,side,senderCh,replyCh,message,distance=os.pullEvent("modem_message")
 modem.transmit(replyCh,1,dest)
